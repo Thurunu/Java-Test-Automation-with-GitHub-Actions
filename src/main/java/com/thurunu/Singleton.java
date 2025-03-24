60c8f7c0a8602d8dd6ef15984315816792307893
Thurunu Mihiranga <86581450+Thurunu@users.noreply.github.com>
2025-03-25 02:00:56 +0530

    Update sigeloton class code to check github action is functioning correctly

--- src/main/java/com/thurunu/Singleton.java
+++ src/main/java/com/thurunu/Singleton.java
@@ -4,7 +4,7 @@ public class Singleton {
 
     private static Singleton instance = null;
 
-    private Singleton() {}
+    public Singleton() {}
 
     public static Singleton getInstance() {
         if(instance == null) {
