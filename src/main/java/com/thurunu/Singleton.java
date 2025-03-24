6001e03420b022f6fa49a2a664e51e23b6bae76b
Thurunu Mihiranga <86581450+Thurunu@users.noreply.github.com>
2025-03-25 02:05:41 +0530

    use upload-artifact to create a report about tests

--- src/main/java/com/thurunu/Singleton.java
+++ src/main/java/com/thurunu/Singleton.java
@@ -4,7 +4,7 @@ public class Singleton {
 
     private static Singleton instance = null;
 
-    public Singleton() {}
+    private Singleton() {}
 
     public static Singleton getInstance() {
         if(instance == null) {
